0eee9bd5e3e648f97948976a809540c1037db189
Angel Esquinas <58303196+Anllel12@users.noreply.github.com>
2023-03-21 19:09:33 +0100

    Add some funtions

--- src/Hash.java
+++ src/Hash.java
@@ -1,3 +1,32 @@
 public class Hash {
-    // Clase para HAsh
+    public Hash(){
+    }
+
+    public Hash(int capacidad){
+    }
+
+    public Hash(int capacidad, float alfaMax){
+    }
+
+    public void insertar(int clave, Valor v){
+    }
+
+    public boolean borrar(int clave){
+        return true;
+    }
+
+    public Valor get(int clave){
+    }
+
+    public boolean esVacia(){
+        return true;
+    }
+
+    public float getAlfaMax(){
+    }
+
+    public void setAlfaMax(float){
+    }
+
+
 }
